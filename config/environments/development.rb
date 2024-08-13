ff628dea54d94d3eab6e81e77b777c58b2fc9621
ltk1903 <khangltk19032002@gmail.com>
2024-08-13 16:46:25 +0700

    add send email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,20 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'localhost',
+  user_name:            'ENV["GMAIL_USERNAME"]',
+  password:             'ENV["GMAIL_PASSWORD"]',
+  authentication:       'plain',
+  enable_starttls_auto: true
+}
+
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
