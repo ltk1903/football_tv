f901b96008e1e14d981ae1beca5f3b46818ac276
ltk1903 <124751492+ltk1903@users.noreply.github.com>
2024-08-13 16:45:28 +0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+  
+  after_create :send_welcome_email
+
+  private
+
+  def send_welcome_email
+    UserMailer.with(user: self).welcome_email.deliver_later
+  end
 end
