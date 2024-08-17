e6b2e0855076b7f45564134994275cc4f4b60f8c
ltk1903 <khangltk19032002@gmail.com>
2024-08-17 10:40:29 +0700

    update send email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,8 @@
 class UserMailer < ApplicationMailer
-    default from: 'khangltk19032002@gmail.com'
+  default from: 'khangltk19032002@gmail.com'
   
-    def welcome_email
-      @user = params[:user]
-      @url  = 'http://example.com/login'
-      mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-    end
+  def welcome_email
+    mail(to: "yanni19032002@gmail.com", subject: 'Welcome to My Awesome Site')
   end
+end
   
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,8 +7,8 @@ config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'localhost',
-  user_name:            'ENV["GMAIL_USERNAME"]',
-  password:             'ENV["GMAIL_PASSWORD"]',
+  user_name:            'khangltk19032002@gmail.com',
+  password:             'kemhmgiftcmtplok',
   authentication:       'plain',
   enable_starttls_auto: true
 }
@@ -51,9 +51,9 @@ config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
