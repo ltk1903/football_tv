40df6c5155e19760a51f30a4b64648e7e9624861
ltk1903 <khangltk19032002@gmail.com>
2024-09-06 09:48:44 +0700

    add sending gmail

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,6 @@ class User < ApplicationRecord
   private
 
   def send_welcome_email
-    UserMailer.with(user: self).welcome_email.deliver_now
+    UserMailer.welcome_email(self).deliver_now
   end
 end
